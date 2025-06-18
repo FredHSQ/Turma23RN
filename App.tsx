36390e7a40b99d96044149d6ca9e91df8d49db7e
FredHSQ <frederico.shere@gmail.com>
2025-06-18 20:53:29 -0300

    First commit

--- App.tsx
+++ App.tsx
@@ -1,20 +1,46 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, Image, TouchableOpacity, TextInput, FlatList } from 'react-native'
+import { styles } from './styles';
+import CicleIcon from './CircleIcon.png';
 
-export default function App() {
+const App = () => {
+  const [skills, setSkills] = useState<string[]>([]);
+  const [newSkill, setNewSkill] = useState<string>('');
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.title}>
+        Bem vindo, Fred
+      </Text>
+      <TextInput
+        style={styles.input}
+        onChangeText={text => {
+          setNewSkill(text)
+        }}
+      />
+      <TouchableOpacity 
+        style={styles.button}
+        onPress={()=> setSkills((oldSkills)=> [...oldSkills, newSkill])}
+      >
+        <Text style={styles.buttonText}>
+          Adiciona Skill
+        </Text>
+      </TouchableOpacity>
+      <FlatList
+        data={skills}
+        keyExtractor={(item, index) => index.toString()}
+        renderItem={({ item }) => {
+          return (
+            <TouchableOpacity style={styles.buttonSkill}>
+              <Image source={CicleIcon} style={styles.image}/>
+              <Text style={styles.textSkill}>
+                { item }
+              </Text>
+            </TouchableOpacity>
+        )}}
+      />
     </View>
-  );
+  )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
